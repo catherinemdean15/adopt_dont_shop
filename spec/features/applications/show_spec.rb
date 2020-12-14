19ab9198d5b0f07e302f3368a42302bb27903220
Catherine Dean <catherinemdean15@gmail.com>
2020-12-14 15:30:46 -0700

    create feature test for application show page

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Applications show page', type: :feature do
+  before :each do
+    @application1 = create(:application)
+  end
+
+  it "displays the applicant information" do
+    visit "applications/#{@application1.id}"
+save_and_open_page
+    expect(page).to have_content(@application1.name)
+    expect(page).to have_content(@application1.street_address)
+    expect(page).to have_content(@application1.city)
+    expect(page).to have_content(@application1.state)
+    expect(page).to have_content(@application1.zip)
+    expect(page).to have_content(@application1.description)
+    expect(page).to have_content(@application1.status)
+  end
+end
