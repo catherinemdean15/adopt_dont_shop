8fd2b7b669d1239d305ac76356dde665837105fd
Catherine Dean <catherinemdean15@gmail.com>
2020-12-14 22:10:17 -0700

    create test for relationships

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Application, type: :model do
+  describe "relationships" do
+    it { should have_many :pet_applications}
+    it { should have_many(:pets).through(:pet_applications)}
+  end
+
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :street_address}
