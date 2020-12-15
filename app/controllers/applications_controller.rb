94dd5e8b35b167ef882bf283ed1aa84af5ee9cea
Catherine Dean <catherinemdean15@gmail.com>
2020-12-14 20:24:50 -0700

    update applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,28 @@
-class ApplicationsController < ActionController::Base
+class ApplicationsController < ApplicationController
+  def index
+    @applications = Application.all
+  end
+
   def show
     @application = Application.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+      application = Application.new(application_params)
+      application[:status] = "In Progress"
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      flash[:notice] = "All fields must be completed"
+      render :new
+    end
+  end
+
+  private
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zip)
   end
 end
