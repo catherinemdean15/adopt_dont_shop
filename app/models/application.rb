d332ca24f424169a489cdd1f92d1c0f730758abf
Catherine Dean <catherinemdean15@gmail.com>
2020-12-17 18:38:38 -0700

    update application to switch alphabetize action to application_record

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,4 +7,22 @@ class Application < ApplicationRecord
                         :state,
                         :zip
 
+
+  def pet_status(pet_id, application_id)
+    PetApplication.find_by(pet_id: pet_id, application_id: application_id).status
+  end
+
+  def app_status
+    if (PetApplication.where(application_id: self.id, status: "Approved").count) == (PetApplication.where(application_id: self.id).count)
+      self.update({
+        status: "Approved"
+        })
+    elsif (PetApplication.where(application_id: self.id, status: "Rejected").count) >= 1
+      self.update({
+        status: "Rejected"
+        })
+    end
+  end
+
+
 end
