e13ee811f351b10d9bc022b449b1a67605ccd196
Catherine Dean <catherinemdean15@gmail.com>
2020-12-17 18:34:46 -0700

    update pets and pet_applications controller

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -4,8 +4,9 @@ class PetApplicationsController < ApplicationController
     if params[:commit] == "Add pet"
       @pet = Pet.find(params[:pet_id])
       @application = Application.find(params[:id])
-      @pet_application = PetApplication.create!(pet_id: @pet.id, application_id: @application.id)
+      @pet_application = PetApplication.create!(pet_id: @pet.id, application_id: @application.id, status: "Pending")
       redirect_to "/applications/#{@application.id}"
     end
   end
+
 end
